d8a67d7eb33f9d21c613dc002c018d045c6687c8
Jean Dalpra <03497876038@senacrs.edu.br>
2026-06-30 09:46:25 -0300

    Ajuste no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <p className="footer-sub">
           Organize seus livros, favoritos e leituras em um só lugar <br /> Para no vestibular Arasar!!!.
         </p>
-        <span id="sobre" className="footer-copy">© 2026 Todos os direitos reservados</span>
+        <span id="sobre" className="footer-copy">© 2026 Todos os direitos reservados.</span>
       </div>
     </footer>
   );
